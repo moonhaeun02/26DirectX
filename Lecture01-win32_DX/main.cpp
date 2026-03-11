2b02b667cee8259925954b374efb91ffde94321c
evil77 <elflee77@gmail.com>
2026-03-11 15:02:50 +0900

    .

---
+++ Lecture01-win32_DX/main.cpp
@@ -0,0 +1,166 @@
+/*
+ * [이론 설명: Win32 API 기본 헤더]
+ * windows.h: 창 생성, 메시지 처리 등 Windows OS 기능을 쓰기 위한 필수 헤더
+ */
+
+#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
+
+#include <windows.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+
+ // 라이브러리 링크
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+// 전역 변수 (간결한 예제를 위해 사용)
+ID3D11Device * g_pd3dDevice = nullptr;                  //모든 리소스의 생성을 담당하는 핵심 객체임. 하드웨어(GPU)와의 통로 역할을 하며, 실질적으로 메모리를 할당하는 기능을 가짐.
+ID3D11DeviceContext* g_pImmediateContext = nullptr;     //생성된 리소스를 사용하여 GPU에 그리기 명령(Rendering Commands)을 내리는 객체임. 파이프라인의 상태를 설정하고 실제로 "그려라(Draw)"라고 지시함.
+IDXGISwapChain* g_pSwapChain = nullptr;                 //그려진 그림을 모니터 화면으로 전달하고 관리하는 시스템임. 더블 버퍼링(Double Buffering) 기술의 실체라고 보면 됨.
+ID3D11RenderTargetView* g_pRenderTargetView = nullptr;  //GPU가 결과물을 써 내려갈 대상(Target)을 정의하는 '뷰(View)' 객체임. DX11에서는 리소스(Texture2D)를 직접 파이프라인에 꽂지 않음. 대신 그 리소스를 어떤 용도(렌더 타겟용, 셰이더 읽기용 등)로 쓸 것인지 정의하는 'View'를 통해 접근함.
+
+// 정점 구조체
+struct Vertex {
+    float x, y, z;
+    float r, g, b, a;
+};
+
+// HLSL 셰이더 (이전 예제와 동일)
+const char* shaderSource = R"(
+struct VS_INPUT { float3 pos : POSITION; float4 col : COLOR; };
+struct PS_INPUT { float4 pos : SV_POSITION; float4 col : COLOR; };
+PS_INPUT VS(VS_INPUT input) {
+    PS_INPUT output;
+    output.pos = float4(input.pos, 1.0f);
+    output.col = input.col;
+    return output;
+}
+float4 PS(PS_INPUT input) : SV_Target { return input.col; }
+)";
+
+/*
+ * [이론 설명: 윈도우 프로시저 (WndProc)]
+ * 운영체제가 윈도우에 보낸 메시지(마우스 클릭, 창 닫기 등)를 처리하는 함수임.
+ */
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_DESTROY: // 창이 닫힐 때 발생하는 메시지
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// --- Win32 메인 진입점 ---
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    // 1. 윈도우 클래스 등록 (창의 속성 정의)
+    WNDCLASSEX wcex = { sizeof(WNDCLASSEX) };
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc; // 메시지 처리 함수 연결
+    wcex.hInstance = hInstance;
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.lpszClassName = L"DX11Win32Class";
+    RegisterClassEx(&wcex);
+
+    // 2. 실제 윈도우 생성
+    HWND hWnd = CreateWindow(L"DX11Win32Class", L"Win32 + DirectX 11 Triangle",
+        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd) return -1;
+    ShowWindow(hWnd, nCmdShow);
+
+    // 3. DirectX 11 초기화 (Win32 HWND 연결)
+    DXGI_SWAP_CHAIN_DESC sd = {};
+    sd.BufferCount = 1;
+    sd.BufferDesc.Width = 800;
+    sd.BufferDesc.Height = 600;
+    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    sd.OutputWindow = hWnd; // 생성한 Win32 창 핸들 연결
+    sd.SampleDesc.Count = 1;
+    sd.Windowed = TRUE;
+
+    D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0, nullptr, 0,
+        D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, nullptr, &g_pImmediateContext);
+
+    // Render Target View 생성
+    ID3D11Texture2D* pBackBuffer = nullptr;
+    g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&pBackBuffer);
+    g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_pRenderTargetView);
+    pBackBuffer->Release();
+
+    // 4. 셰이더 및 버퍼 설정 (이전과 동일한 로직)
+    ID3DBlob* vsBlob, * psBlob;
+    D3DCompile(shaderSource, strlen(shaderSource), nullptr, nullptr, nullptr, "VS", "vs_4_0", 0, 0, &vsBlob, nullptr);
+    D3DCompile(shaderSource, strlen(shaderSource), nullptr, nullptr, nullptr, "PS", "ps_4_0", 0, 0, &psBlob, nullptr);
+
+    ID3D11VertexShader* vShader;
+    ID3D11PixelShader* pShader;
+    g_pd3dDevice->CreateVertexShader(vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), nullptr, &vShader);
+    g_pd3dDevice->CreatePixelShader(psBlob->GetBufferPointer(), psBlob->GetBufferSize(), nullptr, &pShader);
+
+    // Input Layout
+    D3D11_INPUT_ELEMENT_DESC layout[] = {
+        { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+        { "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+    };
+    ID3D11InputLayout* pInputLayout;
+    g_pd3dDevice->CreateInputLayout(layout, 2, vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), &pInputLayout);
+
+    // Vertex Buffer
+    Vertex vertices[] = {
+        {  0.0f,  0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f },
+        {  0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f },
+        { -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f },
+    };
+    ID3D11Buffer* pVBuffer;
+    D3D11_BUFFER_DESC bd = { sizeof(vertices), D3D11_USAGE_DEFAULT, D3D11_BIND_VERTEX_BUFFER, 0, 0, 0 };
+    D3D11_SUBRESOURCE_DATA initData = { vertices, 0, 0 };
+    g_pd3dDevice->CreateBuffer(&bd, &initData, &pVBuffer);
+
+    // [이론 설명: 메시지 루프]
+    // GetMessage는 메시지가 올 때까지 대기(Wait)하지만, 
+    // 게임은 메시지가 없어도 계속 그려야 하므로 PeekMessage를 사용하여 Non-blocking으로 처리함.
+    MSG msg = { 0 };
+    while (WM_QUIT != msg.message) {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else {
+            // --- 렌더링 시작 ---
+            float clearColor[] = { 0.1f, 0.2f, 0.3f, 1.0f };
+            g_pImmediateContext->ClearRenderTargetView(g_pRenderTargetView, clearColor);
+
+            g_pImmediateContext->OMSetRenderTargets(1, &g_pRenderTargetView, nullptr);
+            D3D11_VIEWPORT vp = { 0, 0, 800, 600, 0.0f, 1.0f };
+            g_pImmediateContext->RSSetViewports(1, &vp);
+
+            g_pImmediateContext->IASetInputLayout(pInputLayout);
+            UINT stride = sizeof(Vertex), offset = 0;
+            g_pImmediateContext->IASetVertexBuffers(0, 1, &pVBuffer, &stride, &offset);
+            g_pImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+            g_pImmediateContext->VSSetShader(vShader, nullptr, 0);
+            g_pImmediateContext->PSSetShader(pShader, nullptr, 0);
+
+            g_pImmediateContext->Draw(3, 0);
+            g_pSwapChain->Present(0, 0);
+        }
+    }
+
+    // 자원 해제
+    if (pVBuffer) pVBuffer->Release();
+    if (pInputLayout) pInputLayout->Release();
+    if (vShader) vShader->Release();
+    if (pShader) pShader->Release();
+    if (g_pRenderTargetView) g_pRenderTargetView->Release();
+    if (g_pSwapChain) g_pSwapChain->Release();
+    if (g_pImmediateContext) g_pImmediateContext->Release();
+    if (g_pd3dDevice) g_pd3dDevice->Release();
+
+    return (int)msg.wParam;
+}
