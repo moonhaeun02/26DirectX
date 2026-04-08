20470b83bd6bfd5b8757f91ca99e3130d09e7d08
moonyuwol <moonyuwol@MYWORLD>
2026-04-08 14:41:49 +0900

    윈도우 창 생성, 메세지 핸들러 구현

--- Lecture04-HW/main.cpp
+++ Lecture04-HW/main.cpp
@@ -11,6 +11,40 @@
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3dcompiler.lib")
 
+// 메세지 처리 함수
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    if (message == WM_DESTROY) {
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    return 0;
+    // 윈도우 클래스
+    WNDCLASSEXW wcex = { sizeof(WNDCLASSEX) };
+    wcex.lpfnWndProc = WndProc;
+    wcex.hInstance = hInstance;
+    wcex.lpszClassName = L"DX11Engine";
+    RegisterClassExW(&wcex);
+
+    RECT rc = { 0, 0, 800, 600 };
+    AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+    
+    // 윈도우 창 생성
+    HWND hWnd = CreateWindowW(L"DX11Engine", L"Lecture04 HW - 2 Players", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top,
+        nullptr, nullptr, hInstance, nullptr);
+    ShowWindow(hWnd, nCmdShow);
+
+    // 메세지 루프
+    MSG msg = { 0 };
+    while (WM_QUIT != msg.message) {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int)msg.wParam;
 }
