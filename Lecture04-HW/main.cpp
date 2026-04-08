1d7352751af6a63baab1dc8085d3f396a2d2592c
moonyuwol <moonyuwol@MYWORLD>
2026-04-08 17:05:16 +0900

    셰이더 컴파일

--- Lecture04-HW/main.cpp
+++ Lecture04-HW/main.cpp
@@ -16,12 +16,43 @@ ID3D11Device* g_pd3dDevice = nullptr;
 ID3D11DeviceContext* g_pImmediateContext = nullptr;
 IDXGISwapChain* g_pSwapChain = nullptr;
 ID3D11RenderTargetView* g_pRenderTargetView = nullptr;
-std::vector<GameObject*> g_GameWorld;
+ID3D11VertexShader* g_pVertexShader = nullptr;
+ID3D11PixelShader* g_pPixelShader = nullptr;
+ID3D11InputLayout* g_pInputLayout = nullptr;
 
 struct VideoConfig {
     int Width = 800;
     int Height = 600;
 } g_Config;
+
+struct Vertex {
+    float x, y, z;
+    float r, g, b, a;
+};
+
+// 셰이더
+const char* g_ShaderSource = R"(
+    struct VS_IN { 
+        float3 pos : POSITION; 
+        float4 col : COLOR; 
+    };
+    
+    struct PS_IN { 
+        float4 pos : SV_POSITION; 
+        float4 col : COLOR; 
+    };
+    
+    PS_IN VS(VS_IN input) { 
+        PS_IN output; 
+        output.pos = float4(input.pos, 1.0f); 
+        output.col = input.col; 
+        return output; 
+    }
+    
+    float4 PS(PS_IN input) : SV_Target { 
+        return input.col; 
+    }
+)";
 
 class Component {
 public:
@@ -58,6 +89,8 @@ public:
         components.push_back(pComp);
     }
 };
+
+std::vector<GameObject*> g_GameWorld;
 
 // 메세지 처리 함수
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
@@ -105,6 +138,24 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&pBackBuffer);
     g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_pRenderTargetView);
     pBackBuffer->Release();
+
+    // 셰이더 컴파일
+    ID3DBlob* vsBlob, * psBlob;
+    D3DCompile(g_ShaderSource, strlen(g_ShaderSource), nullptr, nullptr, nullptr, "VS", "vs_4_0", 0, 0, &vsBlob, nullptr);
+    D3DCompile(g_ShaderSource, strlen(g_ShaderSource), nullptr, nullptr, nullptr, "PS", "ps_4_0", 0, 0, &psBlob, nullptr);
+
+    // 셰이더 생성
+    g_pd3dDevice->CreateVertexShader(vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), nullptr, &g_pVertexShader);
+    g_pd3dDevice->CreatePixelShader(psBlob->GetBufferPointer(), psBlob->GetBufferSize(), nullptr, &g_pPixelShader);
+
+    // InputLayout 생성
+    D3D11_INPUT_ELEMENT_DESC layout[] = {
+        { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+        { "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+    };
+    g_pd3dDevice->CreateInputLayout(layout, 2, vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), &g_pInputLayout);
+    vsBlob->Release();
+    psBlob->Release();
 
     // 게임 루프
     MSG msg = { 0 };
@@ -160,7 +211,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         }
     }
 
+    for (auto obj : g_GameWorld) {
+        delete obj;
+    }
+
     // 메모리 정리
+    if (g_pInputLayout) g_pInputLayout->Release();
+    if (g_pVertexShader) g_pVertexShader->Release();
+    if (g_pPixelShader) g_pPixelShader->Release();
     if (g_pRenderTargetView) g_pRenderTargetView->Release();
     if (g_pSwapChain) g_pSwapChain->Release();
     if (g_pImmediateContext) g_pImmediateContext->Release();
